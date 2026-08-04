de62633d1cad72ca1350dae098e3f4c536ae2a7a
andinizharifah <andinizharifah286@gmail.com>
2026-08-04 14:19:00 +0700

    HTML
    UPLOAD HTML

---
+++ html/dasar_html.php
@@ -0,0 +1,27 @@
+<body>
+    <form>
+        nama : <input type="text"name="nama"> <br/>
+        alamat : <input type="text"name="alamat"> </br/>
+        password : <input type="password"name="pass"> <br/>
+        jenis kelamin :
+                <input type="radio"name="jk"value="laki-laki"/>Laki-laki 
+                <input type="radio"name="jk"value="perempuan"/>perempuan 
+                <br/>
+        bahasa pemrograman yang dikuasai :
+                <input type="checkbox"name="web">web
+                <input type="checkbox"name="mobile">MOBILE
+                <input type="checkbox"name="desk">DESKTOP
+                <br/>
+        kota asal :
+        <select>
+            <option>Semarang</option>
+            <option>Bandung</option>
+        </select>
+        <br/>
+        pesan anda :
+        <textaera></textaera>
+        <br/>
+        <input type="submit"value="proses">
+    </form>
+</body>
+</html>
---
+++ html/paragrap_html.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>belajar membuat paragraf</title>
+    </head>
+    <body>
+    <p align="left">1. ini merupakan penulisan paragraf rata kiri dalam dokument HTML </P>
+    <p align="right">1. ini merupakan penulisan paragraf rata kanan dalam dokument HTML </P>
+    <p align="center">1. ini merupakan penulisan paragraf rata tengah dalam dokument HTML </P>
+    <p align="left">1. ini merupakan penulisan paragraf rata kiri kanan dalam dokument HTML </P>
+    </body>
+</html>
